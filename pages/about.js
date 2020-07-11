7e4ca12a98889cbc429946d5068f510cf3c6fcfa
Hunter Raffety <hunter.raffety@clairvoyantsoft.com>
2020-07-11 07:59:25 -0700

    minor content updates

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,45 @@
 import React from 'react'
+
 import BasePage from '../components/layouts/BasePage'
-import { p } from 'prettier'
 
 const About = () => {
   return (
     <BasePage type="about">
       <h1>hello.</h1>
-      <p>my name is hunter.</p>
+      <p>
+        my name is hunter. i drive a dark grey 2015 subaru wrx premium edition.
+        i really like my car, i love driving and working on it, and i also
+        really love to build web apps.
+      </p>
+      <p>
+        i'm currently working on building this app with the goal of it being a
+        corner on the internet for my car and my interests and life, i guess,
+        but hope to turn it in to a place for others who care to share and post
+        their cars and the journey that they're going on with them.
+      </p>
+      <p>
+        i know that facebook has groups, there are sites like clubwrx.net, and
+        nasioc, etc. but they all have their own set of issues.
+        <p>
+          Facebook is great and this site will integrate with it rather than
+          creating an account here, but posts on clubwrx and nasioc and such all
+          decay and deteriorate after time. i plan on being a subaru owner for a
+          long time, and want this space to serve as a repository for some 20
+          year old kid who worked his ass off to buy a subaru wrx finally, and
+          finds this site that is still present and working as he goes on to
+          build things and work on his car.
+        </p>
+        <p>
+          most importantly, this allows me to have a project to pursue to keep
+          building and working on, as I am a software engineer by profession.
+          So, this site is built from the ground up like any other modern web
+          application.
+        </p>
+        <p>
+          if you're interested in some of the tools and technologies that I used
+          to build this site, I will have a section dedicated to that.
+        </p>
+      </p>
     </BasePage>
   )
 }
