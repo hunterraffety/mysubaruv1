648d8bec3347ab2d9ed0c2f5548be30b90ba6cc2
Hunter Raffety <hunter.raffety@clairvoyantsoft.com>
2020-07-11 19:01:43 -0700

    quick

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 const path = require('path')
 module.exports = {
+  // Target must be serverless
+  target: 'serverless',
   webpack: config => {
     config.resolve.alias['@'] = path.resolve(__dirname)
     return config
